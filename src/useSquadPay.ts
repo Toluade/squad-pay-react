85c272fea3102b6d89ba49dfcdc396ed9de4b62d
ToluAdeoye <toluwaloope.adeoye@habaripay.com>
2024-04-27 20:07:16 +0100

    useSquadPay hook

---
+++ src/useSquadPay.ts
@@ -0,0 +1,62 @@
+import { useEffect } from "react";
+
+interface CustomWindow extends Window {
+  squad?: any;
+}
+
+declare var window: CustomWindow;
+
+type SquadPayProps = {
+  key: string;
+};
+
+const useSquadPay = ({ key }: SquadPayProps) => {
+  function squadPay({
+    amount,
+    email,
+    currency_code = "NGN",
+    redirect_link = "https://squadco.com",
+    params,
+    onLoad = () => null,
+    onClose = () => null,
+    onSuccess = () => null,
+  }: {
+    amount: number;
+    email: string;
+    currency_code: "NGN" | "USD";
+    redirect_link: string;
+    params: Record<string, any>;
+    onLoad: () => void;
+    onClose: () => void;
+    onSuccess: () => void;
+  }) {
+    const squadInstance = new window.squad({
+      onClose: () => onClose(),
+      onLoad: () => onLoad(),
+      onSuccess: () => onSuccess(),
+      key,
+      email: email,
+      amount: amount * 100,
+      ...params,
+      currency_code,
+      redirect_link,
+    });
+    squadInstance.setup();
+    squadInstance.open();
+  }
+
+  useEffect(() => {
+    const base_url = "https://checkout.squadco.com";
+    const script = document.createElement("script");
+    script.src = `${base_url}/widget/squad.min.js`;
+    if (document) {
+      document?.head?.appendChild(script);
+    } else {
+      throw new Error("Unable to access DOM");
+    }
+  }, []);
+
+  return squadPay;
+};
+
+export default useSquadPay;
